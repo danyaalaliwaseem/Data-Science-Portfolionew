cbc0dd7684423b952b2268b6a832a7a188be068c
danyaalaliwaseem <danyaalw36@gmail.com>
2026-09-12 14:57:04 -0700

    Personalize greeting with favorite color

--- projects/greeting-project/hello.py
+++ projects/greeting-project/hello.py
@@ -1,7 +1,13 @@
 # My first Python project in GitHub
 def greet_user():
     name = input("Enter your name: ")
+    color = input("What is your favorite color? ")
     print(f"Hello, {name}! Welcome to Data Science.")
+
+    print(f"Hello, {name}! Welcome to Data Science.")
+    print(f"{color} is a great color!")
+
+
 
 if __name__ == "__main__":
     greet_user()
